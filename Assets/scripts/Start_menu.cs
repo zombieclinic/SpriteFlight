dc49ee64f8d127c00714cd6a5e61c03fe4d22b2e
raymn <raymnd10@gmail.com>
2026-01-24 20:21:44 -0500

    sounds and functions to menu

--- Assets/scripts/Start_menu.cs
+++ Assets/scripts/Start_menu.cs
@@ -5,33 +5,57 @@ using UnityEngine.UIElements;
 public class Start_menu : MonoBehaviour
 {
 
-     [Header("Music")]
+    [Header("Music")]
     public AudioSource audioSource;
     public AudioClip[] musicTracks;
 
     public UIDocument uiDocument;
 
     [Header("Buttons")]
-     private Button startButton;
+    private Button startButton;
+    private Button exitButton;
+    public AudioSource ButtonAudio;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-          PlayRandom();
+        PlayRandom();
 
-          var root = uiDocument.rootVisualElement;
-          startButton = root.Q<Button>("StartLabel");
+        var root = uiDocument.rootVisualElement;
+        startButton = root.Q<Button>("StartLabel");
+        exitButton = root.Q<Button>("ExitLabel");
 
-          startButton.clicked += OnStartClicked;
+        startButton.clicked += OnStartClicked;
+        exitButton.clicked += OnExitClicked;
     }
 
     // Update is called once per frame
-   private void OnStartClicked()
-{
-    SceneManager.LoadScene("sprite_flight");
-}
+    private void OnStartClicked()
+    {
+        ButtonAudio.Play();
+        Invoke(nameof(LoadGame), 1f);
+    }
 
+    private void LoadGame()
+    {
+        SceneManager.LoadScene("sprite_flight");
+    }
+
+    private void OnExitClicked()
+    {
+        ButtonAudio.Play();
+        Invoke(nameof(LoadQuit), 1f);
+    }
+
+    private void LoadQuit()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
 
     private void PlayRandom()
     {
